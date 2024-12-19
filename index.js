a1e534b2bc40f73e84c7e27c600c6f8d593a073b
Justin <justin-urbanek@bluewin.ch>
2024-12-19 11:13:03 +0100

    feat: pr action

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import chalk from 'chalk';
 
 const word1 = generate(
   { exactly: 1, minLength: 10, wordsPerString: 1 })[0];
-const word2 = generate();
+const word2 =   generate();
 
 console.log(
   `The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
