f04845cf38e488791f9c947f89b607d8444d7212
Justin <justin-urbanek@bluewin.ch>
2024-12-19 11:09:44 +0100

    feat: pr action

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const word2 = generate();
 console.log(
   `The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
 
-export const sum = (a, b) => a + b;
+export const sum = 
+(a, b) => a + b;
 
 // ESLint errors
 /*
