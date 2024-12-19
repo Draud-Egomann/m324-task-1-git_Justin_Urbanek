713e89f80543a88c3409e2d343e1ab1401221f8f
Justin <justin-urbanek@bluewin.ch>
2024-12-19 10:56:55 +0100

    feat: pr action

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const word1 = generate(
   { exactly: 1, minLength: 10, wordsPerString: 1 })[0];
 const word2 = generate();
 
-console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
+console.log(
+  `The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
 
 export const sum = (a, b) => a + b;
 
